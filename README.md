f4aae25142580ec0157673e4257d0e7de52615aa
Greg Wuller <greg@afofo.com>
2026-08-14 18:36:16 -0700

    Switch dep/loris from the autotools fork to kellyfitz/loris 2.0.
    Upstream now builds with CMake and includes the C++17 fixes we carried in the fork, so compile Loris statically in-tree and drop the ExternalProject/autotools bootstrap.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # utu
 
-`utu` uses the [loris](http://www.cerlsoundgroup.org/Loris/) library to provide
+`utu` uses the [loris](https://github.com/kellyfitz/loris) library to provide
 sound analysis, synthesis, and morphing functionality. `utu` defines an
 alternative JSON based file format for partial data to allow the analysis output
 to be used in a wide number of environments.
@@ -18,20 +18,26 @@ to be used in a wide number of environments.
 
 ### dependencies
 
-`utu` builds and statically links several third party libraries from source
-which requires installation of the GNU autotools suite.
+`utu` builds and statically links several third party libraries from source.
 
 on linux:
 
 ```
-sudo apt install cmake autotools-dev automake autoconf m4 libtool libasound2-dev
+sudo apt install cmake libasound2-dev
 ```
 
 on macos:
 
 ```
-brew install cmake ninja automake autoconf m4 libtool
+brew install cmake ninja
 ```
+
+FFTW 3 is optional but recommended. Without it, Loris uses a bundled FFT and
+infrequent non-power-of-two transforms are slower.
+
+on linux: `sudo apt install libfftw3-dev`
+
+on macos: `brew install fftw`
 
 ### building
 
--- cmake/BuildLoris.cmake
+++ cmake/BuildLoris.cmake
@@ -1,88 +1,118 @@
-include(ExternalProject)
 include(FindPkgConfig)
 
+# Static overlay over kellyfitz/loris v2.0. Do not add_subdirectory(dep/loris):
+# upstream's CMakeLists.txt is a standalone project (shared lib, CTest, Python).
+# Keep the source lists in sync with dep/loris/src/CMakeLists.txt when bumping
+# the submodule.
 set(loris_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/dep/loris)
 
-find_program(AUTOUPDATE_COMMAND NAMES autoupdate)
-find_program(AUTORECONF_COMMAND NAMES autoreconf)
+enable_language(C)
 
 pkg_check_modules(FFTW fftw3 IMPORTED_TARGET)
 
-# The following attempts to work around old crusty autotools, specifically
-#
-# (1) loris configure does not offer a way to specify an alternate prefix for
-# the libraries it wants to leverage, instead one must add set CPPFLAGS/LDFLAGS
-# in order for the configure tests to find the needed bits.
-#
-# (2) Locating fftw is done via pkg-config which will work on macOS (homebrew)
-# and Linux, possibly Windows. If pkg-config finds fftw then we inject the
-# needed bits into the environment when configuring
-#
-# (3) "cmake -E env" can't pass environment variables where the values contain
-# spaces to a subcommand. The suggested workaround was to generate a script and
-# then execute the script.
-#
+# Version macros substituted into src/loris.h.in (v2.0 tag).
+set(LORIS_MAJOR_VERSION 2)
+set(LORIS_MINOR_VERSION 0)
+set(LORIS_SUBMINOR_VERSION 0)
+set(LORIS_PRERELEASE_STR "")
+set(LORIS_VERSION_STR "Loris ${LORIS_MAJOR_VERSION}.${LORIS_MINOR_VERSION}${LORIS_PRERELEASE_STR}")
+
+set(loris_gen_dir ${CMAKE_CURRENT_BINARY_DIR}/loris-gen)
+configure_file(
+  ${loris_SOURCE_DIR}/src/loris.h.in
+  ${loris_gen_dir}/loris.h
+  @ONLY
+)
+
+# Public include layout matching the installed API: <loris/Analyzer.h>
+set(loris_wrap_dir ${CMAKE_CURRENT_BINARY_DIR}/loris-include)
+file(MAKE_DIRECTORY ${loris_wrap_dir})
+file(REMOVE ${loris_wrap_dir}/loris)
+file(CREATE_LINK ${loris_SOURCE_DIR}/src ${loris_wrap_dir}/loris SYMBOLIC)
+
+set(loris_src ${loris_SOURCE_DIR}/src)
+
+set(LORIS_CPP_SRC
+  ${loris_src}/AiffData.cpp
+  ${loris_src}/AiffFile.cpp
+  ${loris_src}/Analyzer.cpp
+  ${loris_src}/AssociateBandwidth.cpp
+  ${loris_src}/BigEndian.cpp
+  ${loris_src}/Breakpoint.cpp
+  ${loris_src}/BreakpointUtils.cpp
+  ${loris_src}/Channelizer.cpp
+  ${loris_src}/Collator.cpp
+  ${loris_src}/Dilator.cpp
+  ${loris_src}/Distiller.cpp
+  ${loris_src}/Envelope.cpp
+  ${loris_src}/F0Estimate.cpp
+  ${loris_src}/LorisExceptions.cpp
+  ${loris_src}/Filter.cpp
+  ${loris_src}/FourierTransform.cpp
+  ${loris_src}/FrequencyReference.cpp
+  ${loris_src}/Fundamental.cpp
+  ${loris_src}/Harmonifier.cpp
+  ${loris_src}/ImportLemur.cpp
+  ${loris_src}/KaiserWindow.cpp
+  ${loris_src}/LinearEnvelope.cpp
+  ${loris_src}/Marker.cpp
+  ${loris_src}/Morpher.cpp
+  ${loris_src}/NoiseGenerator.cpp
+  ${loris_src}/Notifier.cpp
+  ${loris_src}/Oscillator.cpp
+  ${loris_src}/Partial.cpp
+  ${loris_src}/PartialBuilder.cpp
+  ${loris_src}/PartialList.cpp
+  ${loris_src}/PartialUtils.cpp
+  ${loris_src}/phasefix.cpp
+  ${loris_src}/ReassignedSpectrum.cpp
+  ${loris_src}/Resampler.cpp
+  ${loris_src}/SdifFile.cpp
+  ${loris_src}/Sieve.cpp
+  ${loris_src}/SpcFile.cpp
+  ${loris_src}/SpectralPeakSelector.cpp
+  ${loris_src}/SpectralSurface.cpp
+  ${loris_src}/Synthesizer.cpp
+  ${loris_src}/fftsg.c
+)
+
+set(LORIS_PI_SRC
+  ${loris_src}/lorisAnalyzer_pi.cpp
+  ${loris_src}/lorisBpEnvelope_pi.cpp
+  ${loris_src}/lorisException_pi.cpp
+  ${loris_src}/lorisNonObj_pi.cpp
+  ${loris_src}/lorisPartialList_pi.cpp
+  ${loris_src}/lorisUtilities_pi.cpp
+)
+
+set(LORIS_FASTSYNTH_SRC
+  ${loris_src}/fast-synth-src/BlockOscillator.cpp
+  ${loris_src}/fast-synth-src/BlockSynthBwe.cpp
+  ${loris_src}/fast-synth-src/BlockSynthReader.cpp
+  ${loris_src}/fast-synth-src/r250.c
+  ${loris_src}/fast-synth-src/randlcg.c
+)
+
+add_library(loris STATIC ${LORIS_CPP_SRC} ${LORIS_PI_SRC} ${LORIS_FASTSYNTH_SRC})
+target_compile_features(loris PUBLIC cxx_std_17)
+target_compile_definitions(loris PUBLIC FASTSYNTH_FLOAT_TYPE=double)
+target_compile_options(loris PRIVATE -Wno-comment)
+target_include_directories(loris
+  SYSTEM PUBLIC
+    ${loris_wrap_dir}
+  PRIVATE
+    ${loris_gen_dir}
+    ${loris_src}
+    ${loris_src}/fast-synth-src
+)
+
 if(FFTW_FOUND)
-    ExternalProject_Add(loris
-        SOURCE_DIR ${loris_SOURCE_DIR}
-        UPDATE_DISCONNECTED true # do not attempt to update source on rebuild
-        PATCH_COMMAND ${CMAKE_COMMAND} -E echo "CPPFLAGS=\"${FFTW_STATIC_CFLAGS}\" LDFLAGS=\"${FFTW_STATIC_LDFLAGS}\" ${loris_SOURCE_DIR}/configure --prefix=<INSTALL_DIR> --with-python=NO --with-csound=NO --with-utils=NO" > ${CMAKE_CURRENT_BINARY_DIR}/configure-loris.sh
-        CONFIGURE_COMMAND sh ${CMAKE_CURRENT_BINARY_DIR}/configure-loris.sh
-        BUILD_COMMAND make -j${Ncpu}
-        BUILD_BYPRODUCTS ${loris_LIBRARY}
-    )
-else()
-    ExternalProject_Add(loris
-        SOURCE_DIR ${loris_SOURCE_DIR}
-        UPDATE_DISCONNECTED true # do not attempt to update source on rebuild
-        CONFIGURE_COMMAND ${loris_SOURCE_DIR}/configure --prefix=<INSTALL_DIR> --with-python=NO --with-csound=NO --with-utils=NO --with-fftw=NO
-        BUILD_COMMAND make -j${Ncpu}
-        BUILD_BYPRODUCTS ${loris_LIBRARY}
-    )
+  target_compile_definitions(loris PRIVATE HAVE_FFTW3_H=1)
+  target_link_libraries(loris PUBLIC PkgConfig::FFTW)
 endif()
 
-ExternalProject_Get_Property(loris SOURCE_DIR)
-
-ExternalProject_Add_Step(loris
-    autoupdate
-    COMMAND ${AUTOUPDATE_COMMAND}
-    DEPENDEES download
-    WORKING_DIRECTORY ${SOURCE_DIR}
-)
-
-ExternalProject_Add_Step(loris
-    touch-changelog
-    COMMAND touch ChangeLog
-    DEPENDEES download
-    WORKING_DIRECTORY ${SOURCE_DIR}
-)
-
-ExternalProject_Add_Step(loris
-    bootstrap
-    COMMAND ${AUTORECONF_COMMAND} --install
-    DEPENDEES autoupdate touch-changelog
-    DEPENDERS configure
-    WORKING_DIRECTORY ${SOURCE_DIR}
-)
-
-# HACK: create the install directories so that the target_include_directories function does not error during configure.
-file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/loris-prefix/include)  # avoid race condition
-file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/loris-prefix/lib)  # avoid race condition
-
-add_library(loris::loris INTERFACE IMPORTED GLOBAL)
-target_include_directories(loris::loris INTERFACE
-    "${PROJECT_BINARY_DIR}/loris-prefix/include"
-)
-
-set(loris_libs
-    "${PROJECT_BINARY_DIR}/loris-prefix/lib/${CMAKE_STATIC_LIBRARY_PREFIX}loris${CMAKE_STATIC_LIBRARY_SUFFIX}"
-)
-
-# TODO: Figure out how to force static linking of fftw
-if(FFTW_FOUND)
-    # ensure consumers of the loris target link against the needed libraries
-    list(APPEND loris_libs PkgConfig::FFTW)
+if(NOT APPLE)
+  target_link_libraries(loris PRIVATE m)
 endif()
-target_link_libraries(loris::loris INTERFACE ${loris_libs})
 
-add_dependencies(loris::loris loris)
+add_library(loris::loris ALIAS loris)
